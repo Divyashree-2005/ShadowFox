51fc38593d2a87dd346fbcc4964fa8c33b53cc44
Divyashree-2005 <divyadivyashree818@gmail.com>
2025-10-27 15:23:21 +0530

    Add files via upload

---
+++ shadowfox T2/script.js
@@ -0,0 +1,15 @@
+document.getElementById('signinBtn').addEventListener('click', () => {
+  alert('Sign In feature coming soon!');
+});
+
+document.getElementById('orderBtn').addEventListener('click', () => {
+  alert('Redirecting to Order Online page...');
+});
+
+document.getElementById('exploreBtn').addEventListener('click', () => {
+  document.getElementById('products').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('visitBtn').addEventListener('click', () => {
+  alert('Visit our cafe at the nearest location!');
+});
